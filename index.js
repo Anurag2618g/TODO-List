3be8384013bbdc9cbdcb094466c04f0ac917f1fa
Anurag2618g <anurag2618g@gmail.com>
2024-07-02 23:08:25 +0530

    add and get route set

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const db = new pg.Client({
   password: "chained_86",
   port: 5432,
 });
-
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,26 +21,30 @@ let items = [];
 
 app.get("/", async(req, res) => {
   try{
-    const result = db.query("SELECT * FROM items ORDER BY id ASC");
+    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
     items = result.rows;
     res.render("index.ejs", {
       listItems: items,
     });
   } catch (err) {
-    res.sendStatus("Database cannot be accessed");
+    res.statusCode(404);
     res.redirect("/");
   }
 });
 
 app.post("/add", async(req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+  try{
+    await db.query("INSERT INTO items (title) VALUES ($1)",[item]);
+    res.redirect("/");
+  } catch (err) {
+    res.statusCode(404);
+  }
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async(req, res) => {});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async(req, res) => {});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
