76101ab4be529594eebc1fa9e531c92af1b00510
Anurag2618g <anurag2618g@gmail.com>
2024-07-02 22:48:24 +0530

    database connect

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "ToDoList",
+  password: "chained_86",
+  port: 5432,
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+let items = [];
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
-  });
+app.get("/", async(req, res) => {
+  try{
+    const result = db.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+    res.render("index.ejs", {
+      listItems: items,
+    });
+  } catch (err) {
+    res.sendStatus("Database cannot be accessed");
+    res.redirect("/");
+  }
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async(req, res) => {
   const item = req.body.newItem;
   items.push({ title: item });
   res.redirect("/");
