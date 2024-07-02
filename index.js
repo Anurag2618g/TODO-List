1659e3238969b86fadc89e1827d9b48c29658855
Anurag2618g <anurag2618g@gmail.com>
2024-07-02 23:20:57 +0530

    delete route

--- index.js
+++ index.js
@@ -44,7 +44,15 @@ app.post("/add", async(req, res) => {
 
 app.post("/edit", async(req, res) => {});
 
-app.post("/delete", async(req, res) => {});
+app.post("/delete", async(req, res) => {
+  const id = req.body.deleteItemId;
+  try {
+    await db.query("DELETE FROM items WHERE id = ($1)",[id]);
+    res.redirect("/");
+  } catch (err) {
+    res.statusCode(404);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
